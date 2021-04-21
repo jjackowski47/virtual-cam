a383c58d423517e8151e2f03b35024f953ec9073
jjackowski47 <49172089+jjackowski47@users.noreply.github.com>
2021-04-21 21:11:53 +0200

    Add hidden surface elimination

--- camera.py
+++ camera.py
@@ -1,6 +1,7 @@
 from matrices import *
 from operations import *
 import PySimpleGUI as sg
+import numpy as np
 
 sg.theme('DarkGrey2')
 
@@ -12,17 +13,81 @@ def CtrlBtn(button_text):
     return sg.Button(button_text, size=(8, 1), font=("Helvetica", 12))
 
 
+def sort_by_depth(arr):
+    n = len(arr)
+    for i in range(n-1):
+        for j in range(0, n-i-1):
+            if is_closer(arr[j], arr[j+1]):
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+    return arr
+
+
+def is_closer(plane, check):
+    p1 = np.array(plane[0])
+    p2 = np.array(plane[1])
+    p3 = np.array(plane[2])
+
+    cp1 = np.array(check[0]) / 1e5
+    cp2 = np.array(check[1]) / 1e5
+    cp3 = np.array(check[2]) / 1e5
+
+    v1 = p3 - p1
+    v2 = p2 - p1
+
+    cp = np.cross(np.concatenate(v1[:3]), np.concatenate(v2[:3]))
+    a, b, c = cp
+
+    d = np.dot(cp, p3[:3]) / 1e5
+
+    cam_dir = 1 if d < 0 else -1
+
+    tr_dir1 = a*(cp1[0]) + b*(cp1[1]) + c*(cp1[2]) - d
+    tr_dir2 = a*(cp2[0]) + b*(cp2[1]) + c*(cp2[2]) - d
+    tr_dir3 = a*(cp3[0]) + b*(cp3[1]) + c*(cp3[2]) - d
+
+    results = [np.round(x, 6) for x in [tr_dir1, tr_dir2, tr_dir3]]
+
+    for i in range(len(results)):
+        if results[i] > 0:
+            results[i] = 1
+        elif results[i] == 0:
+            results[i] = 0
+        else:
+            results[i] = -1
+
+    if not any(results):
+        return True
+
+    check = [x for x in results if x != 0]
+    if cam_dir * check[0] < 0:
+        return True
+    return False
+
+
 def draw():
-    for i, coord in enumerate(lines_coords):
+    tkcanv.delete('all')
+    triangles_list = sort_by_depth(triangles_coords)
+    for i, coord in enumerate(triangles_list):
+        color = 'lightblue'
+        if coord[3] < 38:
+            color = 'lightgreen'
+        if coord[3] < 25:
+            color = 'pink'
+        if coord[3] < 13:
+            color = 'lightyellow'
         new_coords_A = M.dot(coord[0])
         new_coords_B = M.dot(coord[1])
-
-        tkcanv.coords(lines[i], [
+        new_coords_C = M.dot(coord[2])
+        tkcanv.create_polygon(
             int(new_coords_A[0] / new_coords_A[3]),
             int(new_coords_A[1] / new_coords_A[3]),
             int(new_coords_B[0] / new_coords_B[3]),
-            int(new_coords_B[1] / new_coords_B[3])
-        ])
+            int(new_coords_B[1] / new_coords_B[3]),
+            int(new_coords_C[0] / new_coords_C[3]),
+            int(new_coords_C[1] / new_coords_C[3]),
+            fill=color,
+            outline='black',
+        )
 
 
 Mvp = get_Mvp(CANVAS_WIDTH, CANVAS_HEIGHT)
@@ -31,7 +96,7 @@ Mper = get_Mper(*ovv.values())
 M = Mvp.dot(Mper)
 
 canv = sg.Canvas(size=(CANVAS_WIDTH, CANVAS_HEIGHT),
-                 key='canvas', background_color='black')
+                 key='canvas', background_color='#222')
 
 control_panel = [
     [CtrlBtn('Up')],
@@ -46,6 +111,8 @@ control_panel = [
 layout = [
     [canv, sg.Column(control_panel, element_justification='c')],
     [CtrlBtn('Exit')]
+
+
 ]
 
 window = sg.Window('Virtual Cam', layout, use_default_focus=False,
@@ -55,19 +122,20 @@ window.read(timeout=0)
 
 tkcanv = window['canvas'].TKCanvas
 
-lines = []
-lines_coords = []
+triangles = []
+triangles_coords = []
 with open('coords.txt', 'r') as file:
-    for line in file.readlines():
+    for index, line in enumerate(file.readlines()):
         if line[0] != '\n':
             coords = [int(x) for x in line.split()]
             A = np.array([[*coords[:3], 1]]).T
-            B = np.array([[*coords[3:], 1]]).T
+            B = np.array([[*coords[3:6], 1]]).T
+            C = np.array([[*coords[6:], 1]]).T
 
-            lines_coords.append((A, B))
+            triangles_coords.append((A, B, C, index))
 
-            lines.append(
-                tkcanv.create_line(A[0][0], A[1][0], B[0][0], B[1][0], fill='white'))
+            triangles.append(
+                tkcanv.create_polygon(A[0][0], A[1][0], B[0][0], B[1][0], C[0][0], C[1][0], fill='blue', outline='black'))
 
 draw()
 
@@ -75,46 +143,46 @@ while True:
     event, values = window.read(timeout=0)
     if event is not sg.TIMEOUT_KEY:
         if event in ('Up', 'w'):
-            move(lines_coords, 'down')
+            move(triangles_coords, 'down')
             draw()
         if event in ('Down', 's'):
-            move(lines_coords, 'up')
+            move(triangles_coords, 'up')
             draw()
         if event in ('Left', 'a'):
-            move(lines_coords, 'right')
+            move(triangles_coords, 'right')
             draw()
         if event in ('Right', 'd'):
-            move(lines_coords, 'left')
+            move(triangles_coords, 'left')
             draw()
         if event in ('Front', 'z'):
-            move(lines_coords, 'front')
+            move(triangles_coords, 'front')
             draw()
         if event in ('Back', 'x'):
-            move(lines_coords, 'back')
+            move(triangles_coords, 'back')
             draw()
         if event in ('Rotate Z -', 'q'):
-            rotate(lines_coords, -0.01, 'z')
+            rotate(triangles_coords, -ANGLE_STEP, 'z')
             draw()
         if event in ('Rotate Z +', 'e'):
-            rotate(lines_coords, 0.01, 'z')
+            rotate(triangles_coords, ANGLE_STEP, 'z')
             draw()
         if event in ('Rotate X -', 'c'):
-            rotate(lines_coords, -0.01, 'x')
+            rotate(triangles_coords, -ANGLE_STEP, 'x')
             draw()
         if event in ('Rotate X +', 'v'):
-            rotate(lines_coords, 0.01, 'x')
+            rotate(triangles_coords, ANGLE_STEP, 'x')
             draw()
         if event in ('Rotate Y -', 'b'):
-            rotate(lines_coords, -0.01, 'y')
+            rotate(triangles_coords, -ANGLE_STEP, 'y')
             draw()
         if event in ('Rotate Y +', 'n'):
-            rotate(lines_coords, 0.01, 'y')
+            rotate(triangles_coords, ANGLE_STEP, 'y')
             draw()
         if event in ('Zoom in', 'o'):
-            zoom(lines_coords, 'in')
+            zoom(triangles_coords, 'in')
             draw()
         if event in ('Zoom out', 'p'):
-            zoom(lines_coords, 'out')
+            zoom(triangles_coords, 'out')
             draw()
         if event in (sg.WIN_CLOSED, 'Exit'):
             break
--- operations.py
+++ operations.py
@@ -2,7 +2,8 @@ from tkinter import *
 import numpy as np
 from matrices import *
 
-STEP = 3
+STEP = 30
+ANGLE_STEP = 0.1
 
 DIRECTIONS = {
     'up': [0, -STEP, 0],
@@ -24,7 +25,11 @@ def move(coords, direction):
             [[coord[1][0][0]], [coord[1][1][0]], [coord[1][2][0]], [1]])
         new_cords_B = translate(*DIRECTIONS[direction]).dot(new_cords_B)
 
-        coords[i] = (new_cords_A, new_cords_B)
+        new_cords_C = np.array(
+            [[coord[2][0][0]], [coord[2][1][0]], [coord[2][2][0]], [1]])
+        new_cords_C = translate(*DIRECTIONS[direction]).dot(new_cords_C)
+
+        coords[i] = (new_cords_A, new_cords_B, new_cords_C, coord[3])
 
 
 def rotate(coords, angle, axis):
@@ -33,18 +38,23 @@ def rotate(coords, angle, axis):
             [[coord[0][0][0]], [coord[0][1][0]], [coord[0][2][0]], [1]])
         new_cords_B = np.array(
             [[coord[1][0][0]], [coord[1][1][0]], [coord[1][2][0]], [1]])
+        new_cords_C = np.array(
+            [[coord[2][0][0]], [coord[2][1][0]], [coord[2][2][0]], [1]])
 
         if axis == 'x':
             new_cords_A = rotateX(angle).dot(new_cords_A)
             new_cords_B = rotateX(angle).dot(new_cords_B)
+            new_cords_C = rotateX(angle).dot(new_cords_C)
         elif axis == 'y':
             new_cords_A = rotateY(angle).dot(new_cords_A)
             new_cords_B = rotateY(angle).dot(new_cords_B)
+            new_cords_C = rotateY(angle).dot(new_cords_C)
         elif axis == 'z':
             new_cords_A = rotateZ(angle).dot(new_cords_A)
             new_cords_B = rotateZ(angle).dot(new_cords_B)
+            new_cords_C = rotateZ(angle).dot(new_cords_C)
 
-        coords[i] = (new_cords_A, new_cords_B)
+        coords[i] = (new_cords_A, new_cords_B, new_cords_C, coord[3])
 
 
 def zoom(coords, direction):
@@ -62,4 +72,8 @@ def zoom(coords, direction):
             [[coord[1][0][0]], [coord[1][1][0]], [coord[1][2][0]], [1]])
         new_cords_B = get_Mzoom(z).dot(new_cords_B)
 
-        coords[i] = (new_cords_A, new_cords_B)
+        new_cords_C = np.array(
+            [[coord[2][0][0]], [coord[2][1][0]], [coord[2][2][0]], [1]])
+        new_cords_C = get_Mzoom(z).dot(new_cords_C)
+
+        coords[i] = (new_cords_A, new_cords_B, new_cords_C, coord[3])
